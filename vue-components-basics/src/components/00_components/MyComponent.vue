29f02ecec5bdb95cf8f75e577e65e9ff909e3cf1
zkvn1103@naver.com <zkvn1103@naver.com>
2025-11-19 14:24:15 +0900

    component

---
+++ vue-components-basics/src/components/00_components/MyComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+
+onMounted(() => {
+  console.log('MyComponent 마운트 완료');
+});
+
+</script>
+
+<template>
+  <div>
+    <h1>My Component 입니다.</h1>
+    <h3>단일 컴포넌트 기본 구조</h3>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    color : red;
+  }
+</style>
